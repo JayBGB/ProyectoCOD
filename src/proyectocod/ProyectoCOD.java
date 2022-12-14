77158565a255ec1768339274533668dd98dba8aa
Borja G.B <bgarciabarrera@danielcastelao.org>
2022-12-14 13:34:16 +0100

    First commit

---
+++ src/proyectocod/ProyectoCOD.java
@@ -0,0 +1,16 @@
+
+package proyectocod;
+
+/**
+ *
+ * @author JayBGB
+ */
+public class ProyectoCOD {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world! This is Jay!");
+    }
+    
+}
